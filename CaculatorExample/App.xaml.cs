2c25030a1cc1f253afdd013371830ff1bc7b4554
Shaun Stummer <shaun.stummer1@tafensw.edu.au>
2024-09-26 10:14:28 +1000

    Added Window Resize

--- CaculatorExample/App.xaml.cs
+++ CaculatorExample/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace CaculatorExample
+﻿using Microsoft.Maui.Controls.PlatformConfiguration;
+
+namespace CaculatorExample
 {
     public partial class App : Application
     {
@@ -7,6 +9,19 @@
             InitializeComponent();
 
             MainPage = new AppShell();
+        }
+
+        protected override Window CreateWindow(IActivationState activationState)
+        {
+            Window window = base.CreateWindow(activationState);
+
+            const int newWidth = 550;
+            const int newHeight = 600;
+
+            window.Width = newWidth;
+            window.Height = newHeight;
+
+            return window;
         }
     }
 }
